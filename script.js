f3eb1e4f7459a2e7e95dc0750f979c9f95284243
desenjao <134088421+desenjao@users.noreply.github.com>
2023-11-18 22:27:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    exibirUltimasPalavras();
-});
+ <script>
+        function abrirFormulario() {
+            document.getElementById("formulario").style.display = "block";
+        }
 
-function abrirFormulario() {
-    document.getElementById("formulario").style.display = "block";
-}
-function adicionarPalavra() {
-    var novaPalavra = document.getElementById("termo").value;
-    var novoSignificado = document.getElementById("definicao").value;
+        function fecharFormulario() {
+            document.getElementById("formulario").style.display = "none";
+        }
 
-    if (novaPalavra && novoSignificado) {
-        // Adiciona a nova palavra na lista
-        adicionarNaLista({ termo: novaPalavra, definicao: novoSignificado });
+        function adicionarPalavra() {
+            var termo = document.getElementById("termo").value;
+            var definicao = document.getElementById("definicao").value;
 
-        // Fechar o formulário
-        
-    }
-fecharFormulario();
-}
+            // Criar um item de lista para a nova palavra
+            var novoItem = document.createElement("li");
+            novoItem.innerHTML = "<strong>" + termo + ":</strong> " + definicao;
 
-function fecharFormulario() {
-    document.getElementById("formulario").style.display = "none";
-    // Limpar os campos do formulário ao fechar
-    document.getElementById("termo").value = "";
-    document.getElementById("definicao").value = "";
-}
+            // Adicionar o novo item à lista de palavras
+            document.getElementById("lista-palavras").appendChild(novoItem);
 
-
-
-function adicionarNaLista(palavra) {
-    // Adiciona a palavra à lista na Home
-    exibirPalavraBox("lista-palavras", palavra);
-
-    // Adiciona a palavra à lista em Palavras Adicionadas
-    exibirPalavraBox("lista-todas-palavras", palavra);
-}
-
-function exibirPalavraBox(listaId, palavra) {
-    var listaPalavras = document.getElementById(listaId);
-
-    var novaCaixa = document.createElement("li");
-    novaCaixa.classList.add("termo-caixa");
-
-    var nomePalavra = document.createElement("h3");
-    nomePalavra.innerText = palavra.termo;
-    novaCaixa.appendChild(nomePalavra);
-
-    var significadoPalavra = document.createElement("p");
-    significadoPalavra.innerText = palavra.definicao;
-    novaCaixa.appendChild(significadoPalavra);
-
-    listaPalavras.appendChild(novaCaixa);
-}
-
-function exibirUltimasPalavras() {
-    // Exibe as últimas 5 palavras na Home
-    exibirUltimasPalavrasNaHome();
-
-    // Exibe todas as palavras em Palavras Adicionadas
-    exibirTodasPalavras();
-}
-
-function exibirUltimasPalavrasNaHome() {
-    var palavrasSalvas = obterPalavrasSalvas("lista-palavras");
-    exibirPalavras("lista-palavras", palavrasSalvas.slice(-5));
-}
-
-function exibirTodasPalavras() {
-    var palavrasSalvas = obterPalavrasSalvas("lista-todas-palavras");
-    exibirPalavras("lista-todas-palavras", palavrasSalvas);
-}
-
-function exibirPalavras(listaId, palavras) {
-    var listaPalavras = document.getElementById(listaId);
-    listaPalavras.innerHTML = "";
-
-    palavras.forEach(function (palavra) {
-        exibirPalavraBox(listaId, palavra);
-    });
-}
-
-function obterPalavrasSalvas(listaId) {
-    // Obter as palavras salvas a partir do localStorage
-    var palavrasSalvas = localStorage.getItem(listaId);
-
-    // Se não houver palavras salvas, retorna um array vazio
-    return palavrasSalvas ? JSON.parse(palavrasSalvas) : [];
-}
-
-function salvarPalavras(listaId, palavras) {
-    // Salva as palavras no localStorage
-    localStorage.setItem(listaId, JSON.stringify(palavras));
-}
-
-// Chame essa função para iniciar com as palavras já salvas
-exibirUltimasPalavras();
+            // Fechar o formulário após adicionar a palavra
+            fecharFormulario();
+        }
